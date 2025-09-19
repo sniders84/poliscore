bee105cf8c73800f0c4af3e0af288dbd9308f397
sniders84 <sniders1984@gmail.com>
2025-09-19 01:16:11 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import CompareOfficials from './components/CompareOfficials';
 import officials from './officialsMeta_Master.json';
-.compare-grid {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 12px;
-}
 
 function App() {
   return (
